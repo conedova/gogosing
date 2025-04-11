ef5491de9b4925ca2db80feda36df6dc0b6f27c1
heiro <dev.heiro@gmail.com>
2025-04-11 17:12:35 +0900

    chore : 어플리케이션 초기화

---
+++ src/main/java/org/codenova/gogosing/GogosingApplication.java
@@ -0,0 +1,13 @@
+package org.codenova.gogosing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GogosingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GogosingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/codenova/gogosing/GogosingApplicationTests.java
@@ -0,0 +1,13 @@
+package org.codenova.gogosing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GogosingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
