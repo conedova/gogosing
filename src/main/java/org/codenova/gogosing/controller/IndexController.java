1b47b7f66694c4a743d47dd7929dac05dc09234e
heiro <dev.heiro@gmail.com>
2025-04-11 18:14:58 +0900

    fix : 리퀘스트오류수정

--- src/main/java/org/codenova/gogosing/controller/IndexController.java
+++ src/main/java/org/codenova/gogosing/controller/IndexController.java
@@ -1,8 +1,9 @@
 package org.codenova.gogosing.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-
+@RequestMapping("/auth")
 public class IndexController {
 }
