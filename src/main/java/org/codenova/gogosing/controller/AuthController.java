1cccc8db7a2e7b97420d908f579cf1be250adb9c
heiro <dev.heiro@gmail.com>
2025-04-14 10:10:37 +0900

    fix : 롬복

--- src/main/java/org/codenova/gogosing/controller/AuthController.java
+++ src/main/java/org/codenova/gogosing/controller/AuthController.java
@@ -1,8 +1,12 @@
 package org.codenova.gogosing.controller;
 
 
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
+@AllArgsConstructor
 public class AuthController {
+
 }
